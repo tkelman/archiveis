cdcbd831e89c767832ba9034ee5308d335e5b742
Tony Kelman <tony@kelman.net>
2019-10-28 11:53:56 -0700

    how about archive.md

--- archiveis/api.py
+++ archiveis/api.py
@@ -19,13 +19,13 @@ async def do_post(
     Returns the POST response object
     """
     # Put together the URL that will save our request
-    domain = "http://archive.is"
+    domain = "http://archive.md"
     save_url = urljoin(domain, "/submit/")
 
     # Configure the request headers
     headers = {
         'User-Agent': user_agent,
-        "host": "archive.is",
+        "host": "archive.md",
     }
 
     # Request a unique identifier for our activity
